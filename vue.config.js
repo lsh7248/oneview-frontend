101bd3f8cd70f13f81d012092d661319e07d8788
10151032 <s-hyun.lee@kt.com>
2022-05-24 09:54:24 +0900

    vue.config 개발서버 localhost:8000 설정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: "http://localhost:8000",
+  },
+  outputDir: "dist",
+  publicPath: "/",
+  assetsDir: "static",
 });
